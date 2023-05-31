e1bca72285e2ec8a3f7e4b0efb9fa3bddda22091
juan sebastian correa <j.s.correa@videoamp.com>
2023-05-30 20:30:32 -0500

    continuing working on the calc logic

--- google-calc/google.functions.js
+++ google-calc/google.functions.js
@@ -1,5 +1,25 @@
 let previousValue;
 let operation;
+const ADD = "+";
+const SUBSTRACT = "-";
+const MULTIPLY = "*";
+const DIVIDE = "/";
+
+function isOperation(value){
+    if (value=="+"){
+        return ADD;
+    }
+    if (value=="-"){
+        return SUBSTRACT;
+    }
+    if (value=="*"){
+        return MULTIPLY;
+    }
+    if (value=="/"){
+        return DIVIDE;
+    }
+    return null;
+}
 
 function clearScreen(){
     document.getElementById("display").value = "";
@@ -7,8 +27,25 @@ function clearScreen(){
     operation = null;
 }
 
-function updateDisplay(value){
-    document.getElementById("display").value += value;
+/*
+Si previousvalue esta vacio y me envian un digito: concateno
+Si me envian una operacion: guarde primer numero y operacion, muestre operacion
+Si previousvalue NO esta vacio y operacion no esta vacia: concateno
+*/
+function updateDisplay(digit){
+    if (previousValue==undefined && isOperation(digit)==null){
+        document.getElementById("display").value = document.getElementById("display").value + digit;
+    }
+    else if (isOperation(digit)!=null){
+        previousValue = document.getElementById("display").value;
+        operation = digit;
+        document.getElementById("display").value = digit;
+    }
+    else {
+        document.getElementById("display").value = document.getElementById("display").value + digit;
+    }
+
+
 }
 
 function deleteLastValue(){
@@ -16,9 +53,24 @@ function deleteLastValue(){
 }
 
 function equal(){
-    document.getElementById("display").value = eval(document.getElementById("display").value);
-    document.getElementById("log").value = `${previousValue} | ${operation} texto  ${document.getElementById("display").value}`;
+    document.getElementById("log").value = `${previousValue} ${operation} ${document.getElementById("display").value}`;
+
+    if (operation==ADD){
+        document.getElementById("display").value = Number(previousValue) + Number(document.getElementById("display").value);
+    }
+    if (operation==SUBSTRACT){
+        document.getElementById("display").value = Number(previousValue) - Number(document.getElementById("display").value);
+    }
+    if (operation==MULTIPLY){
+        document.getElementById("display").value = Number(previousValue) * Number(document.getElementById("display").value);
+    }
+    if (operation==DIVIDE){
+        document.getElementById("display").value = Number(previousValue) / Number(document.getElementById("display").value);
+    }
+    //document.getElementById("display").value = 
 }
+
+
 
 
 
